2a7c677d6cc37279191c2fc996703436bbdd0d2e
Pedro Duarte <contact@peduarte.com>
2019-08-27 12:43:56 +0100

    Add MDXProvider wrapper and map named exports

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { MDXProvider } from "@mdx-js/react";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { Button as BlueButton } from "../components/blue-button";
+import { Button as RedButton } from "../components/red-button";
+
+export const components = {
+  BlueButton,
+  RedButton,
+}
 
 export default ({ data }) => {
   const post = data.mdx;
 
   return (
-    <div>
-      <h1>{post.frontmatter.title}</h1>
-      <MDXRenderer>{post.body}</MDXRenderer>
-    </div>
+    <MDXProvider components={components}>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <MDXRenderer>{post.body}</MDXRenderer>
+      </div>
+    </MDXProvider>
   );
 };
 
